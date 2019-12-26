2b74d24bf58ef8849951da4dbe0bee4b9fa8eca0
Jack Ogina <jackogina60@gmail.com>
2019-12-26 09:00:17 +0300

    fix typo , added comments

--- b2c.go
+++ b2c.go
@@ -2,18 +2,29 @@ package mpesa
 
 //B2CPayload api payload
 type B2CPayload struct {
-	//username of the API operator in the M-Pesa Org Portal
+	//InitiatorNames is the credential/username used to
+	//authenticate the transaction request.
 	InitiatorName string `json:"InitiatorName"`
-	//password of the API operator encrypted using the public key certificate provided.
+	//SecurityCredential is the Base64 encoded string of the
+	//B2C short code and password, which is encrypted using
+	//M-Pesa public key and validates the transaction on M-Pesa Core system.
 	SecurityCredential string `json:"SecurityCredential"`
-	CommandID          string `json:"CommandID"`
-	Amount             string `json:"Amount"`
-	PartyA             string `json:"Amount"`
-	PartyB             string `json:"PartyB"`
-	Remarks            string `json:"Remarks"`
-	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
-	ResultURL          string `json:"ResultURL"`
-	Occassion          string `json:"Occassion"`
+	//CommandID	Unique command for each transaction type
+	//e.g. SalaryPayment, BusinessPayment, PromotionPayment
+	CommandID string `json:"CommandID"`
+	//Amount The amount being transacted
+	Amount string `json:"Amount"`
+	//PartyA is the Organization’s shortcode initiating the transaction.
+	PartyA string `json:"PartyA"`
+	//PartyB Phone number receiving the transaction
+	PartyB string `json:"PartyB"`
+	//Remarks Comments that are sent along with the transaction.
+	Remarks string `json:"Remarks"`
+	//QueueTimeOutURL The timeout end-point that receives a timeout response.
+	QueueTimeOutURL string `json:"QueueTimeOutURL"`
+	//ResultURL	The end-point that receives the response of the transaction
+	ResultURL string `json:"ResultURL"`
+	Occassion string `json:"Occassion"`
 }
 
 //B2CRes response payload
